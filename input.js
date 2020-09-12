1ea559abccfe174d6534fa7f7344580e80d882f0
aoifemurfe <aoifemurfe@gmail.com>
2020-09-12 16:49:43 +0000

    autocompete

--- input.js
+++ input.js
@@ -1,8 +1,35 @@
 let map;
+let autocomplete;
+let myLatlng = autocomplete;
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: 47.782, lng: -31.727 },
+    zoom: 4
   });
+  var  autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete"),
+    { types: ['establishment'],
+        componentRestrictions: {'country': ['IE']},
+        fields: ['placeid','geometry','name']
+});
+
+  places = new google.maps.places.PlacesService(map);
+  autocomplete.addListener("place_changed", onPlaceChanged);
+}
+
+function onPlaceChanged() {
+  const place = autocomplete.getPlace();
+    var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map
+  });
+
+  if (place.geometry) {
+    map.panTo(place.geometry.location);
+    map.setZoom(15);
+    search();
+  } else {
+    document.getElementById("autocomplete").placeholder = "Enter a city";
+  }
 }
