fe959800e284ff52696a7ccfad545c8a99ff1181
aoifemurfe <aoifemurfe@gmail.com>
2020-09-21 20:33:42 +0000

    adding input options

--- assets/js/input.js
+++ assets/js/input.js
@@ -9,7 +9,7 @@ function initMap() {
   });
   autocomplete = new google.maps.places.Autocomplete(
       document.getElementById("autocomplete"),
-    { types: ['establishment'],
+    { 
         componentRestrictions: {'country': ['IE']},
         fields: ['place_id','geometry','name']
 });
@@ -27,10 +27,10 @@ function onPlaceChanged() {
     map: map
   });
   const cityCircle = new google.maps.Circle({
-      strokeColor: "#FF0000",
+      strokeColor: "#F03939",
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: "#FF0000",
+      fillColor: "#F03939",
       fillOpacity: 0.35,
       map,
       center: place.geometry.location,
