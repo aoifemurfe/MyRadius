aac4fa45740cb668d813a154bd0e4e6a9a57e94c
aoifemurfe <aoifemurfe@gmail.com>
2020-09-27 16:59:37 +0000

    geolocate and search

--- assets/js/input.js
+++ assets/js/input.js
@@ -5,6 +5,7 @@ let markers = [];
 let newradius = 5000;
 let searchfor = [];
 let zoomlevel = 12;
+var service;
 
 
 function initMap() {
@@ -20,12 +21,12 @@ function initMap() {
 
   places = new google.maps.places.PlacesService(map);
   SearchButton.addListener("click", onSearch());
+  infowindow = new google.maps.InfoWindow();
  
 };
 
 
-
-
+//// serach button actions///
 function onSearch() {
    
   const place = autocomplete.getPlace();
@@ -52,13 +53,61 @@ function onSearch() {
     map.setZoom(zoomlevel);
         //search();
   } else {
-    document.getElementById("autocomplete").placeholder = "Enter a location";
+    alert("Enter a location");
+    //document.getElementById("autocomplete").placeholder = "Enter a location";
   }
 
-};
+ var request = {
+    location: place.geometry.location,
+    radius: newradius,
+    query: ['Hospital']
+  };
 
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, markers);
 
+function markers(request, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}}
 
+function createMarker(place) {
+
+    const image = {
+      url: "http://maps.google.com/mapfiles/kml/pal4/icon63.png" ,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25),
+    };
+    new google.maps.Marker({
+        position: place.geometry.location,
+        map: map,
+        icon: image,
+        title: place.name,
+        animation: google.maps.Animation.DROP
+    });
+    google.maps.event.addListener(markers[i], "click", showInfoWindow);
+}
+
+function showInfoWindow() {
+  const marker = this;
+  places.getDetails(
+    { placeId: marker.placeResult.place_id },
+    (place, status) => {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        return;
+      }
+      infoWindow.open(map, marker);
+      buildIWContent(place);
+    }
+  );
+}
+
+  
 function updateradius1() {
 
     newradius = Number(document.getElementById("fivek").value);
@@ -84,4 +133,20 @@ function updatesearch1() {
       console.log(searchfor);
 };
 
- console.log(document.querySelectorAll('input[type=checkbox]:checked'))
+// console.log(document.querySelectorAll('input[type=checkbox]:checked'))
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle(
+          {center: geolocation, radius: position.coords.accuracy});
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
