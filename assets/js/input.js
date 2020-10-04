52922b8aa1d6cb209bd719755174bc4a2e21405f
aoifemurfe <aoifemurfe@gmail.com>
2020-10-04 17:35:39 +0000

    clear results

--- assets/js/input.js
+++ assets/js/input.js
@@ -5,98 +5,111 @@ let newradius = 5000;
 let searchfor = [];
 let zoomlevel = 12;
 let service;
-let marker = [];
+let markers = [];
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 47.782, lng: -31.727 },
-    zoom: 4
-  });
-  autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById("autocomplete"),
-    { 
-        fields: ['place_id','geometry','name']
-});
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 47.782, lng: -31.727 },
+        zoom: 4
+    });
+    
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById("autocomplete"),{ 
+            fields: ['place_id','geometry','name']
+        });
 
-  places = new google.maps.places.PlacesService(map);
-  
-  SearchButton.addListener("click", onSearch());
- 
+    places = new google.maps.places.PlacesService(map);
+    
+    SearchButton.addListener("click", onSearch());
+    ClearButton.addListener("click", onClear());
 };
 
 
-//// serach button actions///
+    //// serach button actions///
 function onSearch() {
+        
+    const cplace = autocomplete.getPlace();
     
-  const cplace = autocomplete.getPlace();
-  
-    marker = new google.maps.Marker({
-    position: cplace.geometry.location,
-    map: map
-   
-  });
-    cityCircle = new google.maps.Circle({
-      strokeColor: "#F03939",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#F03939",
-      fillOpacity: 0.35,
-      map,
-      center: cplace.geometry.location,
-      radius: newradius
-    });
+        marker = new google.maps.Marker({
+        position: cplace.geometry.location,
+        map: map
+        });
 
-  if (cplace.geometry) {
-    marker.setMap(map);
-    map.panTo(cplace.geometry.location);
-    map.setZoom(zoomlevel);
+        circle = new google.maps.Circle({
+        strokeColor: "#F03939",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#F03939",
+        fillOpacity: 0.35,
+        map,
+        center: cplace.geometry.location,
+        radius: newradius
+        });
+
+    if (cplace.geometry) {
+        marker.setMap(map);
+        map.panTo(cplace.geometry.location);
+        map.setZoom(zoomlevel);
+        
+    } else {
+        alert("Enter a location");
+        //document.getElementById("autocomplete").placeholder = "Enter a location";
+    }
+
+    var request = {
+        location: cplace.geometry.location,
+        radius: newradius,
+        types: ['hospital']
+    };
     
-  } else {
-    alert("Enter a location");
-    //document.getElementById("autocomplete").placeholder = "Enter a location";
-  }
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, searchmarkers);
+    infowindow = new google.maps.InfoWindow();
+    console.log(markers)
 
- var request = {
-    location: cplace.geometry.location,
-    radius: newradius,
-    types: ['hospital']
-  };
-  
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, markers);
-  infowindow = new google.maps.InfoWindow();
-
-function markers (results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);}
+    function searchmarkers (results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+            markers.push(createMarker(results[i]));
+            }
         }
     }
+}
+
 function createMarker(s_place) {
 
-    const image = {
-      url: "http://maps.google.com/mapfiles/kml/paddle/blu-stars.png" ,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25),
-    };
-    marker = new google.maps.Marker({
-        position: s_place.geometry.location,
-        map: map,
-        icon: image,
-        title: s_place.name,
-        animation: google.maps.Animation.DROP
-    });
-     marker.addListener("click", function () {
-        
-        infowindow.setContent(s_place.name);  
-        infowindow.setPosition(s_place.geometry.location); 
-        infowindow.open(map, this);
-    })
+   const image = {
+     url: "http://maps.google.com/mapfiles/kml/paddle/blu-stars.png" ,
+     size: new google.maps.Size(70, 80),
+   };
+   smarker = new google.maps.Marker({
+       position: s_place.geometry.location,
+       map: map,
+       icon: image,
+       title: s_place.name,
+       animation: google.maps.Animation.DROP
+   });
+    smarker.addListener("click", function () {
+
+       infowindow.setContent(s_place.name);  
+       infowindow.setPosition(s_place.geometry.location); 
+       infowindow.open(map, this);
+   });
+   return smarker
+}   
+
+
+function onClear() {
+
+    marker.setMap(null);
+    circle.setMap(null);
+    for (var m in markers) {
+        markers[m].setMap(null);
+    }
+    markers = [];
 }
 
-}
+
 function updateradius1() {
 
     newradius = Number(document.getElementById("fivek").value);
