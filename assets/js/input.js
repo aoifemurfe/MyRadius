b159a6c3052c9ee83b9bdf8388d86a7630c854c8
aoifemurfe <aoifemurfe@gmail.com>
2020-10-18 17:59:33 +0000

    remove clear button

--- assets/js/input.js
+++ assets/js/input.js
@@ -6,7 +6,8 @@ let searchfor = [];
 let zoomlevel = 13;
 let service;
 let markers = [];
-
+let circle;
+let cmarker;
 
 
 function initMap() {
@@ -29,16 +30,34 @@ function initMap() {
 
     //// serach button actions///
 function onSearch() {
-        
-    const cplace = autocomplete.getPlace();
+  
+    ///clear old markers if they are there/////
+  if (!markers) {} 
+  
+  else{
+    for (var m in markers) {
+    markers[m].setMap(null);
+    };
+    markers = [];
+    searchfor = [];
+}
+   
     
+//// get the place from the autocomplete box///////
+    const cplace = autocomplete.getPlace();  
+
+
+/// place the circle and center marker on the map or update the previous circle and center marker if performing a second search/////
+
+    if (!cmarker) {
         cmarker = new google.maps.Marker({
         position: cplace.geometry.location,
         map: map
-        });
+        });}
+        else {cmarker.setPosition(cplace.geometry.location);}
       
         
-
+if (!circle) {
         circle = new google.maps.Circle({
         strokeColor: "#F03939",
         strokeOpacity: 0.8,
@@ -48,24 +67,23 @@ function onSearch() {
         map,
         center: cplace.geometry.location,
         radius: newradius
-        });
+        });}
+     else {circle.setRadius(newradius);
+    circle.setCenter(cplace.geometry.location)}
+    
         
-    if (cplace.geometry) {
+    if (cplace) {
  
         map.panTo(cplace.geometry.location);
         map.setZoom(zoomlevel);
-  
-        
-    } else {
-        alert("Enter a location");
-        //document.getElementById("autocomplete").placeholder = "Enter a location";
-    }
+    } 
+    else {alert("enter a location");}
     
     var searchfor1 = document.querySelectorAll('input[type=checkbox]:checked');
     Array.from(searchfor1).forEach(function(el) {
         searchfor.push(el.id)    
     });
-
+//// search requests for each of the types of establishments in the checkbox section. ///////
 
     var request1 = {
         location: cplace.geometry.location,
@@ -92,6 +110,7 @@ function onSearch() {
     };
 
     service = new google.maps.places.PlacesService(map);
+///// if the checkbox for the estalishment type is selected then perform the search for those establishments in the radius bounds and place makers /////
 
     if (!searchfor[0]) {}
     else {
@@ -120,7 +139,7 @@ function onSearch() {
         }
     }
 }
-
+///// function to create markers of results to be placed on the map ///////
 
 function createMarker(s_place) {
 
@@ -146,25 +165,7 @@ const image = {
 }   
 
 
-function onClear() {
-    
-
-    for (var m in markers) {
-        markers[m].setMap(null);
-    };
-
-    markers = [];
-    searchfor = [];
-    document.getElementById('autocomplete').value = ''
-    document.getElementById("supermarket").checked = false;
-    document.getElementById("hospital").checked = false;
-    document.getElementById("pharmacy").checked = false;
-    document.getElementById("doctor").checked = false;
-    circle.setMap(null);
-    cmarker.setMap(null);
-}
-
-// 
+////// update the circle radius size and the zoom level when the radius buttons are selected ////////
 
 function updateradius1() {
 
